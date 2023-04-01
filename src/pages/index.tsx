aef07f0c8915490c4b7e19fbac214e7a27e2fd7a
jonrusso <russovfx@icloud.com>
2023-04-01 01:47:24 -0300

    add: data 📈

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from '@components/Header';
 import AddTaskForm from '@components/AddTaskForm';
+import TasksContainer from '@components/TasksContainer/TasksContainer';
+
+const tasks = [
+  {
+    id: 1,
+    description: "Buy groceries",
+    completed: false,
+  },
+  {
+    id: 2,
+    description: "Go to the gym",
+    completed: true,
+  },
+];
 
 export default function TodoApp() {
   return (
     <>
-    <Header />
-    <AddTaskForm />
+      <Header />
+      <AddTaskForm />
+      <TasksContainer tasks={tasks} />
     </>
-  )
+  );
 }
