d7ca29796357afe39954876216eaf72b6b5744af
jonrusso <russovfx@icloud.com>
2023-04-04 20:01:01 -0300

    fix: import, renaming, styles and div

--- src/components/TasksContainer/TasksContainer.tsx
+++ src/components/TasksContainer/TasksContainer.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Info } from './Tasks/Info';
 import { TaskList, Task } from './Tasks/TaskList';
-
+import styles from './TasksContainer.module.css';
 
 export default function TasksContainer() {
-  const created = 0;
-  const concluded = 0;
+  const created = 3;
+  const concluded = 1;
+
+  const tasks: Task[] = [
+    {
+      id: 1,
+      content: "Buy groceries",
+      completed: false,
+    },
+    {
+      id: 2,
+      content: "Go to the gym",
+      completed: true,
+    },
+    {
+      id: 3,
+      content: "Read 1 chapter of a book",
+      completed: false,
+    },
+  ];
+
   return (
-    <>
+    <div className={styles.tasksContainer}>
       <Info created={created} concluded={concluded} />
-      <TaskList tasks={TaskListProps} />
-    </>
+      <TaskList tasks={tasks} />
+    </div>
   );
 }
