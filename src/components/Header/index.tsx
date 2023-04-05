428c08d7fb320e11ee5c513eac235cd7d33c60eb
jonrusso <russovfx@icloud.com>
2023-04-05 01:01:21 -0300

    fix: logo src 🎨

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <img className={styles.logo} src="/logo.svg" alt="my-todo-logo" />
+      <img className={styles.logo} src="/logo-todo-list.svg" alt="my-todo-logo" />
     </header>
   )
 }
